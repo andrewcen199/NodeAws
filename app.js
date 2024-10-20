c5a9a02579b6d0a89e6522644118bdc16b0e48f1
andrew Centeno <andrewcen166@gmail.com>
2024-10-20 09:59:24 -0400

    added a unch of files

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('first commit');
+const express = require('express');
+// express app
+const app = express();
+
+// listen for requests
+app.listen(3000);
+
+app.get('/', (req, res) => {
+    // res.send('<p>home page</p>');
+    res.sendFile('./views/index.html', { root: __dirname });
+  });
+
+app.get('/about', (req, res) => {
+    // res.send('<p>home page</p>');
+    res.sendFile('./views/about.html', { root: __dirname });
+});
